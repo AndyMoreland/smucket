dda304e81d49b9cda9a79e7a23e43329b25210f3
Andrew Moreland <andy@andymo.org>
2010-11-04 08:51:58 -0700

    Fixes.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
                  [org.clojure/clojure-contrib "1.2.0-beta1"]
-                 [clj-time "0.1.0-RC1"]]
+                 [clj-time "0.1.0-RC1"]
+                 [congomongo "0.1.3-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   )
