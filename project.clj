dbadd2ad23d5e9cbd336f9cc7da451a6bf0d2bc9
Andrew Moreland <andy@andymo.org>
2010-11-05 19:50:03 -0700

    Lots of changes.

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
                  [org.clojars.sids/htmlcleaner "2.1"]
                  [commons-lang "2.5"]
                  [congomongo "0.1.3-SNAPSHOT"]]
-                 ]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   )
---
+++ src/desktop/webloc.clj
@@ -0,0 +1,46 @@
+(ns desktop.webloc.core
+  (:use somnium.congomongo
+        [clojure.xml :as xml]
+        [clojure.zip :as zip]
+        [clojure.contrib.zip-filter.xml :as zf] )
+  (:require [clojure.contrib.string :as st])
+  (:import java.io.File
+           java.util.Date
+           [org.htmlcleaner HtmlCleaner SimpleXmlSerializer]
+           [org.apache.commons.lang StringEscapeUtils]))
+
+
+(defn initialize-cleaner-props [cleaner]
+  "Takes an HTMLCleaner object and makes it ignore comments, script tags, and style tags"
+  (doto (.getProperties cleaner)
+    (.setOmitComments true)
+    (.setPruneTags "script,style"))
+  )
+
+
+(defn isolate-tag [source, tag]
+  (str (.getText (.findElementByName
+                  (let [cleaner (HtmlCleaner.)]
+                    (initialize-cleaner-props cleaner)
+                    (.clean cleaner source)) tag true))))
+
+(defn extract-url-from-webloc [file]
+  (first (zf/xml-> (zip/xml-zip (xml/parse file)) :dict :string zf/text))
+  )
+
+(defn source-for [url]
+  "Returns cleaned html source given url"
+  (let [
+        cleaner (HtmlCleaner.)
+        props (initialize-cleaner-props cleaner)
+        node (.clean cleaner (slurp "http://www.google.com"))
+        serializer (SimpleXmlSerializer. props)
+        ]
+    (.getXmlAsString serializer node))
+  )
+
+(defn process-webloc [file]
+  (let [url (extract-url-from-webloc file)]
+    (isolate-tag (slurp url) "body")
+    )
+  )
